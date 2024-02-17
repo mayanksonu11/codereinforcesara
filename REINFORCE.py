23a2e6e625406389c1b658f0659751d5af9f9c7f
mayanksonu11 <mayanksonu11@gmail.com>
2024-02-17 16:59:14 +0530

    Implementation of delay

---
+++ REINFORCE.py
@@ -0,0 +1,181 @@
+#import gym
+import collections
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+#Hyperparameters
+learning_rate = 0.0002
+gamma         = 0.98
+
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.data = []
+        
+        self.fc1 = nn.Linear(9, 128)
+        self.fc2 = nn.Linear(128, 30)
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.softmax(self.fc2(x), dim=0)
+        return x
+      
+    def put_data(self, item):
+        self.data.append(item)
+        
+    def train_net(self):
+        R = 0
+        self.optimizer.zero_grad()
+        for r, prob in self.data[::-1]:
+            R = r + gamma * R
+            loss = -torch.log(prob) * R
+            loss.backward()
+        self.optimizer.step()
+        self.data = []
+
+# def main():
+#     env = gym.make('CartPole-v1')
+#     pi = Policy()
+#     score = 0.0
+#     print_interval = 20
+    
+    
+#     for n_epi in range(10000):
+#         s, _ = env.reset()
+#         done = False
+        
+#         while not done: # CartPole-v1 forced to terminates at 500 step.
+#             prob = pi(torch.from_numpy(s).float())
+#             m = Categorical(prob)
+#             a = m.sample()
+#             s_prime, r, done, truncated, info = env.step(a.item())
+#             pi.put_data((r,prob[a]))
+#             s = s_prime
+#             score += r
+            
+#         pi.train_net()
+        
+#         if n_epi%print_interval==0 and n_epi!=0:
+#             print("# of episode :{}, avg score : {}".format(n_epi, score/print_interval))
+#             score = 0.0
+#     env.close()
+
+class Agent(object):
+    """Deep Q-learning agent."""
+
+    def __init__(self,
+                 state_space_size,
+                 action_space_size,
+                 path="",
+                 target_update_freq=100, #1000, #cada n steps se actualiza la target network
+                 discount=0.99,
+                 batch_size=32,
+                 max_explore=1,
+                 min_explore=0.05,
+                 anneal_rate=(1/5000), #100000),
+                 replay_memory_size=100000,
+                 replay_start_size= 500): #500): #10000): #despues de n steps comienza el replay
+        """Set parameters, initialize network."""
+        self.action_space_size = action_space_size
+        
+        self.q = Policy()
+        # self.optimizer = optim.Adam(self.q.parameters(), lr=learning_rate)
+        if(path != ""):
+            self.q.load_state_dict(torch.load(path))
+            self.q.eval()
+        self.q_target = Policy()
+        self.q_target.load_state_dict(self.q.state_dict())
+        self.score=0
+
+        #self.online_network = Network(state_space_size, action_space_size)##########Qnet se change karna h
+        #self.target_network = Network(state_space_size, action_space_size)#############
+
+        # self.update_target_network()
+
+        # training parameters
+        self.target_update_freq = target_update_freq
+        self.discount = discount
+        self.batch_size = batch_size
+
+        # policy during learning
+        self.max_explore = max_explore + (anneal_rate * replay_start_size)
+        self.min_explore = min_explore
+        self.anneal_rate = anneal_rate
+        self.steps = 0
+
+        # # replay memory
+        # self.memory = ReplayBuffer(replay_memory_size)
+        # self.replay_start_size = replay_start_size
+        # self.experience_replay = ReplayBuffer(replay_memory_size)
+
+    def handle_episode_start(self):
+        self.last_state, self.last_action = None, None
+
+    def step(self, state, reward, training=True):###############step function me apis se change karne h and call fuction dekhna h
+        
+        """Observe state and rewards, select action.
+        It is assumed that `observation` will be an object with
+        a `state` vector and a `reward` float or integer. The reward
+        corresponds to the action taken in the previous step.
+        """
+
+        last_state, last_action = self.last_state, self.last_action
+        last_reward = reward
+        state = state
+
+        epsilon = max(0.01, 0.08 - 0.01*(self.steps/200)) #Linear annealing from 8% to 1%
+        print_interval = 20
+        print(state)
+
+        prob = self.q(torch.from_numpy(np.asarray(state)).float())
+        self.m = Categorical(prob) 
+        action = self.m.sample()      
+        # if last_state!=None:
+        #     self.memory.put((last_state,last_action,last_reward/100.0,state))
+       
+       
+                   
+       # if self.memory.size()>2000:
+        self.q.put_data((reward,prob[action]))
+
+        self.score += reward
+        self.q.train_net()
+
+        if self.steps%print_interval==0 and self.steps!=0:
+            self.q_target.load_state_dict(self.q.state_dict())
+            print("n_episode :{}, score : {:.1f}, eps : {:.1f}%".format(
+                                                            self.steps, self.score/print_interval, epsilon*100))
+            self.score = 0.0
+        self.steps += 1
+
+        self.last_state = state
+        self.last_action = action
+
+        return action
+
+    # def policy(self, state, training):
+    #     """Epsilon-greedy policy for training, greedy policy otherwise."""
+    #     explore_prob = self.max_explore - (self.steps * self.anneal_rate)#probabilidad de exploracion decreciente
+    #     explore = max(explore_prob, self.min_explore) > np.random.rand()
+
+    #     if training and explore: #hacer exploracion
+    #         action = np.random.randint(self.action_space_size)
+    #     else: #hacer explotacion
+    #         inputs = np.expand_dims(state, 0)
+    #         qvalues = self.online_network.model(inputs) #online or evalation network predicts q-values
+    #         #print("***##qvalues",qvalues)
+    #         action = np.squeeze(np.argmax(qvalues, axis=-1))#need work
+
+    #     return action
+    def save(self):
+        torch.save(self.q.state_dict(),"./reinforce.pt")
+
+    
+# if __name__ == '__main__':
+#     main()
---
+++ ddpg.py
@@ -0,0 +1,265 @@
+import gym
+import random
+import collections
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+#Hyperparameters
+lr_mu        = 0.0005
+lr_q         = 0.001
+gamma        = 0.99
+batch_size   = 32
+buffer_limit = 50000
+tau          = 0.005 # for target network soft update
+
+class ReplayBuffer():
+    def __init__(self,max_size):
+        self.buffer = collections.deque(maxlen=max_size)
+
+    def put(self, transition):
+        self.buffer.append(transition)
+    
+    def sample(self, batch_size):
+        """Sample a batch of experiences from the buffer."""
+        buffer_size = len(self.buffer)
+        print("**",buffer_size)
+        index = np.random.choice(np.arange(buffer_size), size=batch_size, replace=False)
+        return [self.buffer[i] for i in index]
+    
+    def size(self):
+        return len(self.buffer)
+
+class MuNet(nn.Module):
+    def __init__(self,state_space_size,action_space_size):
+        super(MuNet, self).__init__()
+        self.fc1 = nn.Linear(state_space_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc_mu = nn.Linear(64, action_space_size)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        mu = torch.tanh(self.fc_mu(x))*2 # Multipled by 2 because the action space of the Pendulum-v0 is [-2,2]
+        return mu
+
+class QNet(nn.Module):
+    def __init__(self,state_space_size,action_space_size):
+        super(QNet, self).__init__()
+        self.fc_s = nn.Linear(state_space_size, 64)
+        self.fc_a = nn.Linear(action_space_size,64)
+        self.fc_q = nn.Linear(128, 32)
+        self.fc_out = nn.Linear(32,1)
+
+    def forward(self, x, a):
+        h1 = F.relu(self.fc_s(x))
+        h2 = F.relu(self.fc_a(a))
+        cat = torch.cat((h1,h2), dim=0)
+        q = F.relu(self.fc_q(cat))
+        q = self.fc_out(q)
+        return q
+
+class OrnsteinUhlenbeckNoise:
+    def __init__(self, mu):
+        self.theta, self.dt, self.sigma = 0.1, 0.01, 0.1
+        self.mu = mu
+        self.x_prev = np.zeros_like(self.mu)
+
+    def __call__(self):
+        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
+                self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
+        self.x_prev = x
+        return x
+      
+
+    
+
+    
+class Memory(object):
+    """Memory buffer for Experience Replay."""
+
+    def __init__(self, max_size):
+        """Initialize a buffer containing max_size experiences."""
+        self.buffer = cns.deque(maxlen=max_size)
+
+    def add(self, experience):
+        """Add an experience to the buffer."""
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        """Sample a batch of experiences from the buffer."""
+        buffer_size = len(self.buffer)
+        print("**",buffer_size)
+        index = np.random.choice(np.arange(buffer_size), size=batch_size, replace=False)
+        return [self.buffer[i] for i in index]
+
+    def __len__(self):
+        """Interface to access buffer length."""
+        return len(self.buffer)
+
+class Agent(object):
+    def __init__(self,
+                state_space_size,
+                action_space_size,
+                target_update_freq=100, #1000, #cada n steps se actualiza la target network
+                discount=0.99,
+                batch_size=32,
+                max_explore=1,
+                min_explore=0.05,
+                anneal_rate=(1/5000), #100000),
+                replay_memory_size=100000,
+                replay_start_size= 500): #500): #10000): #despues de n steps comienza el replay
+        """Set parameters, initialize network."""
+        self.action_space_size = action_space_size
+
+        self.q, self.q_target = QNet(state_space_size,action_space_size), QNet(state_space_size,action_space_size)
+        self.q_target.load_state_dict(self.q.state_dict())
+        self.mu, self.mu_target = MuNet(state_space_size,action_space_size), MuNet(state_space_size,action_space_size)
+        self.mu_target.load_state_dict(self.mu.state_dict())
+
+        self.mu_optimizer = optim.Adam(self.mu.parameters(), lr=lr_mu)
+        self.q_optimizer  = optim.Adam(self.q.parameters(), lr=lr_q)
+        self.ou_noise = OrnsteinUhlenbeckNoise(mu=np.zeros(action_space_size))
+
+        # self.update_target_network()
+
+        # training parameters
+        self.target_update_freq = target_update_freq
+        self.discount = discount
+        self.batch_size = batch_size
+
+        # policy during learning
+        self.max_explore = max_explore + (anneal_rate * replay_start_size)
+        self.min_explore = min_explore
+        self.anneal_rate = anneal_rate
+        self.steps = 0
+
+        # replay memory
+        self.memory = ReplayBuffer(replay_memory_size)
+        self.replay_start_size = replay_start_size
+        self.experience_replay = ReplayBuffer(replay_memory_size)
+
+    def handle_episode_start(self):
+        self.last_state, self.last_action = None, None
+
+    def step(self, state, reward, training=True):
+        """Observe state and rewards, select action.
+        It is assumed that `observation` will be an object with
+        a `state` vector and a `reward` float or integer. The reward
+        corresponds to the action taken in the previous step.
+        """
+        last_state, last_action = self.last_state, self.last_action
+        last_reward = reward
+        state = state
+        
+        action = self.policy(state, training)
+
+        if training:
+            self.steps += 1
+            print("## step:",self.steps)
+
+            if last_state is not None:
+                experience = {
+                    "state": last_state,
+                    "action": last_action,
+                    "reward": last_reward,
+                    "next_state": state
+                }
+
+                self.memory.put(experience)
+
+            if self.steps > self.replay_start_size: #para acumular cierta cantidad de experiences antes de comenzar el entrenamiento
+                self.train(self.mu, self.mu_target, self.q, self.q_target, self.memory, self.q_optimizer, self.mu_optimizer)
+
+                if self.steps % self.target_update_freq == 0: #el clon de la red se realiza cada cierta cant de steps
+                    self.soft_update(self.mu, self.mu_target)
+                    self.soft_update(self.q,  self.q_target)
+
+        self.last_state = state
+        self.last_action = action
+
+        return np.argmax(action.detach().numpy())
+
+    def policy(self, state, training):
+        """Epsilon-greedy policy for training, greedy policy otherwise."""
+        state = np.array(state)
+        action =  self.mu(torch.from_numpy(state).float()) 
+        for i in range(len(action)):
+            action[i] = action[i] + self.ou_noise()[i]
+        return action
+
+    def soft_update(self, net, net_target):
+        for param_target, param in zip(net_target.parameters(), net.parameters()):
+            param_target.data.copy_(param_target.data * (1.0 - tau) + param.data * tau)
+
+    def train(self, mu, mu_target, q, q_target, memory, q_optimizer, mu_optimizer):
+        batch  = memory.sample(batch_size)
+        inputs = np.array([b["state"] for b in batch]) #####
+        actions = [b["action"] for b in batch]
+        rewards = np.array([b["reward"] for b in batch])
+        next_inputs = np.array([b["next_state"] for b in batch])
+        for i in range(len(batch)):
+            s = torch.tensor(inputs[i])
+            a = torch.tensor(actions[i])
+            r = torch.tensor(rewards[i])
+            s_prime = torch.tensor(next_inputs[i])
+
+            target = r + gamma * q_target(s_prime, mu_target(s_prime))
+            q_loss = F.smooth_l1_loss(q(s,a), target.detach())
+            q_optimizer.zero_grad()
+            q_loss.backward()
+            q_optimizer.step()
+            
+            mu_loss = -q(s,mu(s)).mean() # That's all for the policy loss.
+            mu_optimizer.zero_grad()
+            mu_loss.backward()
+            mu_optimizer.step()
+
+    def save(self):
+        return
+
+def main():
+    env = gym.make('Pendulum-v0')
+
+    memory = ReplayBuffer()
+
+    q, q_target = QNet(), QNet()
+    q_target.load_state_dict(q.state_dict())
+    mu, mu_target = MuNet(), MuNet()
+    mu_target.load_state_dict(mu.state_dict())
+
+    score = 0.0
+    print_interval = 20
+
+    mu_optimizer = optim.Adam(mu.parameters(), lr=lr_mu)
+    q_optimizer  = optim.Adam(q.parameters(), lr=lr_q)
+    ou_noise = OrnsteinUhlenbeckNoise(mu=np.zeros(1))
+
+    for n_epi in range(10000):
+        s = env.reset()
+        done = False
+        
+        while not done:
+            a = mu(torch.from_numpy(s).float()) 
+            a = a.item() + ou_noise()[0]
+            s_prime, r, done, info = env.step([a])
+            memory.put((s,a,r/100.0,s_prime,done))
+            score += r
+            s = s_prime
+                
+        if memory.size()>2000:
+            for i in range(10):
+                train(mu, mu_target, q, q_target, memory, q_optimizer, mu_optimizer)
+                soft_update(mu, mu_target)
+                soft_update(q,  q_target)
+        
+        if n_epi%print_interval==0 and n_epi!=0:
+            print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
+            score = 0.0
+
+    env.close()
+
+if __name__ == '__main__':
+    main()
